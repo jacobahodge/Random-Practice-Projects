8c5f2697cda295c7dd362a3c12dc72da90d0e0f7
jacobahodge <153308257+jacobahodge@users.noreply.github.com>
2024-01-26 11:02:04 -0500

    Update If, elif and else loops.py

--- If, elif and else loops.py
+++ If, elif and else loops.py
@@ -9,4 +9,22 @@ def age_group():
     else:
         print("You are a baby")
 
-age_group()
+def longest_name(name1, name2, name3):
+    print("name1 = " + name1)
+    print("name2 = " + name2)
+    print("name3 = " + name3)
+    if len(name1) > len(name2) and len(name1) > len(name3):
+        return name1
+    elif len(name2) > len(name1) and len(name2) > len(name3):
+        return name2
+    elif len(name3) > len(name1) and len(name3) > len(name2):
+        return name3
+    elif len(name1) == len(name2):
+        print(name1 + " is the same length as " + name2)
+    elif len(name2) == len(name3):
+        print(name2 + " is the same length as " + name3)
+    elif len(name1) == len(name3):
+        print(name1 + " is the same length as " + name3)
+    else:
+        print("error")
+
